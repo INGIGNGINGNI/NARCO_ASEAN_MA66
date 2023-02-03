00bf8253c3ae495045cf330f16f672c120b26d33
Ingkamon Poolnual <95464610+INGIGNGINGNI@users.noreply.github.com>
2023-02-03 09:05:26 +0700

    Update and rename comtop_ing.php to comtop.php

--- comtop.php
+++ comtop.php
@@ -6,8 +6,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="shortcut icon" href="assets/images/logo-narco.png">
 		<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="assets/css/bootstrap_ing.css">
-		<link rel="stylesheet" href="assets/css/style_ing.css">
+		<link rel="stylesheet" href="assets/css/bootstrap.css">
+		<link rel="stylesheet" href="assets/css/style.css">
 
 		<title> ASEAN-NARCO-INDEX </title>
 	</head>
