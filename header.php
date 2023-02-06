b05e07d51cdc17c661e6e5d7c09b17c23c0c5428
INGIGNGINGNI <ingkamon16764daivo@gmail.com>
2023-02-06 08:57:17 +0700

    Edit 6/2/66

--- header.php
+++ header.php
@@ -10,7 +10,8 @@
                 </div>
                 <div class="col-lg-12 p-0 d-flex justify-content-end">
                     <form role="search">
-                        <input type="search" class="form-control" placeholder="    ค้นหาสิ่งที่คุณสนใจ" aria-label="Search">
+                        <input type="search" class="form-control" placeholder="    ค้นหาสิ่งที่คุณสนใจ"
+                            aria-label="Search">
                     </form>
                 </div>
             </div>
@@ -23,76 +24,79 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="container pr-0">
-    <div class="collapse navbar-collapse py-2" id="navbarNavDropdown">
-        <ul class="navbar-nav justify-content-between">
-            <li class="nav-item">
-                <a class="nav-link pl-0" href="#">หน้าหลัก <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                    เกี่ยวกับเรา
-                </a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">ตราสัญลักษณ์</a>
-                    <a class="dropdown-item" href="#">ประวัติ</a>
-                    <a class="dropdown-item" href="#">วิสัยทัศน์/พันธกิจ</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                    ข้อมูล ป.ป.ส.อาเซียน
-                </a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">กำเนิดอาเซียน</a>
-                    <a class="dropdown-item" href="#">ข้อมูลพื้นฐาน</a>
-                    <a class="dropdown-item" href="#">โครงสร้างหน่วยงาน</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                    โครงการสำคัญ
-                </a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">โครงการสกัดกั้นและปราบปรามยาเสพติด ณ ท่าอากาศยานสากล ในอาเซียน</a>
-                    <a class="dropdown-item" href="#">โครงการแก้ปัญหายาเสพติดในสามเหลี่ยมทองคำ</a>
-                    <a class="dropdown-item" href="#">ด้านปราบปรามยาเสพติด</a>
-                    <a class="dropdown-item" href="#">ด้านป้องกันยาเสพติด</a>
-                    <a class="dropdown-item" href="#">ด้านบำบัดรักษาและพื้นฟูสมรรถภาพ</a>
-                    <a class="dropdown-item" href="#">ด้านวิจัย</a>
-                    <a class="dropdown-item" href="#">ด้านพัฒนาทางเลือก</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                    รายงานเฝ้าระวังยาเสพติด
-                </a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">ด้านลดอุปสงค์</a>
-                    <a class="dropdown-item" href="#">ด้านลดอุปทาน</a>
-                    <a class="dropdown-item" href="#">ภาพการประชุม ADMN2019</a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                    รายงาน
-                </a>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">สถิติจับกุม</a>
-                    <a class="dropdown-item" href="#">รายงานประจำปี</a>
-                    <a class="dropdown-item" href="#">สถานการณ์ปัญหายาเสพติด</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">ข่าวสาร</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">ติดต่อเรา</a>
-            </li>
-        </ul>
-    </div>
-<<<<<<< HEAD
+        <div class="collapse navbar-collapse py-2" id="navbarNavDropdown">
+            <ul class="navbar-nav justify-content-between">
+                <li class="nav-item">
+                    <a class="nav-link pl-0" href="#">หน้าหลัก <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                        aria-expanded="false">
+                        เกี่ยวกับเรา
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">ตราสัญลักษณ์</a>
+                        <a class="dropdown-item" href="#">ประวัติ</a>
+                        <a class="dropdown-item" href="#">วิสัยทัศน์/พันธกิจ</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                        aria-expanded="false">
+                        ข้อมูล ป.ป.ส.อาเซียน
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">กำเนิดอาเซียน</a>
+                        <a class="dropdown-item" href="#">ข้อมูลพื้นฐาน</a>
+                        <a class="dropdown-item" href="#">โครงสร้างหน่วยงาน</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                        aria-expanded="false">
+                        โครงการสำคัญ
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">โครงการสกัดกั้นและปราบปรามยาเสพติด ณ ท่าอากาศยานสากล
+                            ในอาเซียน</a>
+                        <a class="dropdown-item" href="#">โครงการแก้ปัญหายาเสพติดในสามเหลี่ยมทองคำ</a>
+                        <a class="dropdown-item" href="#">ด้านปราบปรามยาเสพติด</a>
+                        <a class="dropdown-item" href="#">ด้านป้องกันยาเสพติด</a>
+                        <a class="dropdown-item" href="#">ด้านบำบัดรักษาและพื้นฟูสมรรถภาพ</a>
+                        <a class="dropdown-item" href="#">ด้านวิจัย</a>
+                        <a class="dropdown-item" href="#">ด้านพัฒนาทางเลือก</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                        aria-expanded="false">
+                        รายงานเฝ้าระวังยาเสพติด
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">ด้านลดอุปสงค์</a>
+                        <a class="dropdown-item" href="#">ด้านลดอุปทาน</a>
+                        <a class="dropdown-item" href="#">ภาพการประชุม ADMN2019</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                        aria-expanded="false">
+                        รายงาน
+                    </a>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="#">สถิติจับกุม</a>
+                        <a class="dropdown-item" href="#">รายงานประจำปี</a>
+                        <a class="dropdown-item" href="#">สถานการณ์ปัญหายาเสพติด</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ข่าวสาร</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ติดต่อเรา</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
-=======
 </nav>
->>>>>>> 64987370507af7d5875628d49a141a666e422058
